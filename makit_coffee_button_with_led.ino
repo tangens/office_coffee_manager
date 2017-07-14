7b241a831cf69332686051876168cfd00917e92b
Tangens <tangens88@gmail.com>
2017-07-14 15:00:56 +0300

    .ino with two leds, LCD display and time reset button

---
+++ makit_coffee_button_with_led.ino
@@ -0,0 +1,68 @@
+#include <LiquidCrystal_I2C.h>
+
+const int buttonPin = 3; 
+const int ledPinRed =  11;
+const int ledPinGreen =  10;
+
+int buttonState = 0;      
+
+long startTime; 
+unsigned long previousMillis=0;
+//long interval = 86400000; // 24 hours
+long interval = 72000000; // 20 hours
+//long interval = 3600000 ; // 1 hour
+//long interval = 10000 ; // 10 sec
+
+LiquidCrystal_I2C lcd(0x3F, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  
+
+void setup() {
+  pinMode(ledPinRed, OUTPUT);
+  pinMode(ledPinGreen, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  lcd.begin(20, 4);
+  lcd.setCursor(0, 0); 
+}
+
+void loop() {
+  startTime = millis();
+  
+  lcdprinttime(startTime, previousMillis);
+
+  if ((unsigned long)(startTime - previousMillis) <= interval) {
+    digitalWrite(ledPinGreen, HIGH);
+    digitalWrite(ledPinRed, LOW);
+  } else {
+    digitalWrite(ledPinGreen, LOW);
+    digitalWrite(ledPinRed, HIGH);
+  }
+  
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    previousMillis = startTime;
+    digitalWrite(LED_BUILTIN, HIGH);
+  } else{
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  delay(100);
+}
+
+void lcdprinttime(long startTime, unsigned long previousMillis) {
+  lcd.clear();
+  lcd.print((long)(startTime - previousMillis)/3600000); //hours
+  lcd.print(":");
+  if ((unsigned long)(startTime - previousMillis)/60000 % 60 < 10) {
+    lcd.print(0);
+  }
+  lcd.print((long)(startTime - previousMillis)/60000 % 60); //minutes
+  lcd.print(":");
+  if ((unsigned long)(startTime - previousMillis)/1000 % 60 < 10) {
+    lcd.print(0);
+  }
+  lcd.print((unsigned long)(startTime - previousMillis)/1000 % 60); //seconds
+}
+
+
